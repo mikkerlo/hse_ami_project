501da2780a455d2e05ff86707ea14562c93c4590
Solonkov Denis <solonkovda@gmail.com>
2019-04-03 14:22:42 +0300

    Basic refactoring of the code.
    Changed blank/null logic of the fields to be more consistent, changed logic of the secret key generation to be more useful.

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,9 +1,7 @@
 """
-Description of objects we will operate with.
+This module contains all models of the project.
 
-Student is a person, described by its credentials. Group is a set of Students that attends to a
-specific learning course, where this course is unique for every group. Content element is an
-information that is important for a specific group.
+See each class docstring to more details
 """
 
 from django.db import models
@@ -12,7 +10,7 @@ from django.db.models import Model
 
 class Student(Model):
     """
-    Student description class. Holds basic information about our system user.
+    This class represents the user of our system.
 
     Fields:
         first_name       (string):   First name of person
@@ -22,82 +20,90 @@ class Student(Model):
         email            (string):   Email of person at edu.hse.ru domain
         telegram_account (string):   Telegram login, with @ sign, for example: "@john_smith"
     """
-    first_name = models.CharField(max_length=255, blank=False, null=False)
-    last_name = models.CharField(max_length=255, blank=False, null=False)
-    patronymic_name = models.CharField(max_length=255, blank=False, null=True)
-    birth_date = models.DateField(blank=False, null=False)
-    email = models.EmailField(blank=True, null=False)
-    telegram_account = models.CharField(max_length=255, blank=False, null=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    patronymic_name = models.CharField(max_length=255, blank=True)
+    birth_date = models.DateField()
+    email = models.EmailField(blank=True)
+    telegram_account = models.CharField(max_length=255, blank=True)
 
 
 class Group(Model):
     """
-    Group description class. Holds basic information about user groups.
+    This class represents a collection of users, attending the same course.
+
+    It is important to highlight, that this class doesn't specify academic
+    group, but instead a collection of students attending the same course
+    at the same time.
+    Therefore, one student can have multiple groups and each course can have
+    multiple groups dedicated to it.
     
     Fields:
-        full_name   (string):  Group full name, is shown in group description page     
-        short_name  (string):  Group short name, is used to search group in database
-        description (string):  Group description, arbitrary text, providen by group creator
-        is_hidden   (bool):    Flag, if is true, group is not shown on search result page
-        sudent      (Student): Ids of students that are linked to this group
+        full_name   (string):  Group full name, shown in group description page
+        short_name  (string):  Group short name, гsed to search group in database
+        description (string):  Group description, arbitrary text provided by group creator
+        is_hidden   (bool):    Flag, if is true group is not shown on search result page
+        students      (Student): Ids of students that are linked to this group
     
     """
-    full_name = models.CharField(max_length=255, blank=False, null=False)
-    short_name = models.CharField(max_length=255, blank=False, null=False)
-    description = models.TextField(null=True)
-    is_hidden = models.BooleanField(null=False)
-    sudent = models.ManyToManyField(Student)
+    full_name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_hidden = models.BooleanField()
+    students = models.ManyToManyField(Student)
 
 
 class File(Model):
     """
-    Infromation about uploaded files into system.
+    This class represents a file that can be attached to a content element.
 
     Fields:
         url  (string): URL of file, starting at root of server, for example,
         '/<api_endpoint>/example.txt'
-        name (string): Name of such a file
+        name (string): Name of a file, corresponding to a name in file storage
     """
-    url = models.URLField(blank=False, null=False)
-    name = models.CharField(max_length=4096, blank=False, null=False)
+    url = models.URLField()
+    name = models.CharField(max_length=4096)
 
 
 class ContentElement(Model):
     """
-    Base class for three main content elements of system: Notifications, Materials and Hometasks.
+    Base class for three main content elements of system: Notifications, Materials and Homeworks.
+
+    This class represents an information entry that may be created in a group
+    to be displayed to its students.
 
     Fields:
-        group_id     (id):       id of group for which the element is linked
-        created_at   (datetime): Moment of time when the element is created
-        header       (string):   Header or title of element
-        content      (string):   Main content of elmeent
-        content_file (file):     Attached files, if any are providen
+        group_id     (id):       Id of element's group.
+        created_at   (datetime): Creation timestamp
+        header       (string):   Header of element
+        content      (string):   Main content of element
+        content_file (file):     Attached files if any are provided
 
     """
-    group_id = models.ForeignKey(Group, on_delete=models.CASCADE, blank=False, null=False)
-    created_at = models.DateTimeField(blank=False, null=False)
-    header = models.TextField(blank=False, null=False)
-    content = models.TextField(blank=True, null=False)
-    content_file = models.ManyToManyField(File, blank=False, null=True)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    header = models.TextField()
+    content = models.TextField(blank=True)
+    content_file = models.ManyToManyField(File, blank=True)
 
-    class Base:
+    class Meta:
         abstract = True
 
 
 class Material(ContentElement):
     """
-    Material specialization of ContentElement class
+    This class represents some useful material relevant to the course.
     """
     pass
 
 
-class Hometask(ContentElement):
+class Homework(ContentElement):
     """
-    Hometask specialization of ContentElement class. Contains additional field for holding
-    deadline time.
+    This class represents a given task with a set deadline.
 
     Fields:
-        valid_until (datetime): Moment in time after which task is no longer relevant.
+        valid_until (datetime): Deadline expiration absolute timestamp.
 
     """
     valid_until = models.DateTimeField()
@@ -105,21 +111,21 @@ class Hometask(ContentElement):
 
 class Notification(ContentElement):
     """
-    Notification specialization of ContentElement class.
+    This class represents an important announcement about the course.
     """
     pass
 
 
 class StudentJar(Model):
     """
-    Class for jar of students. Used to store lists of students for ease of navigation and group
+    This class stores list of students for ease of navigation and group
     manipulation. Can be created by any user in system.
 
     Fields:
-        name (string): Name of student jar, used in search. Must be unique.
+        name (string): Name of student jar, used in search.
         created_by (student id): Id of student that created this jar.
-        student_id (student id): Id of students that are linked to this jar.
+        students (student id): Id of students that are linked to this jar.
     """
-    name = models.CharField(max_length=255)
-    created_by = models.ForeignKey(Student)
-    student_id = models.ManyToManyField(Student, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    created_by = models.ForeignKey(Student, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student, on_delete=models.CASCADE)
