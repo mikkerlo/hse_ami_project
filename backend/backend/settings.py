15762c68ec39481311700a63b7c95f4329c5ea6a
Solonkov Denis <solonkovda@gmail.com>
2019-04-06 12:02:11 +0300

    Changed toggling debug flag logic to environ value.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = get_or_create_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if 'DEBUG' in os.environ:
+if os.environ.get('AMI_BACKEND_DEBUG', '0') == '1':
     DEBUG = True
 else:
     DEBUG = False
