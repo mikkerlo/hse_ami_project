182d68747baf0d8f93c6cec5b3605842de4f234e
Solonkov Denis <solonkovda@gmail.com>
2019-04-14 20:36:48 +0300

    Implement students section of the api.
    Change-Id: Id06a34e9d533d404b83c8919fe7e6abb04041333

---
+++ backend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('students/all/', views.students_all, name='api_students_all'),
+    path('students/<int:id>/', views.student_view, name='api_students_id'),
+    path('students/new', views.student_new, name='api_students_new'),
+    path('students/<int:id>/deadlines', views.student_deadlines, name='api_students_deadlines'),
+path('students/<int:id>/groups', views.student_groups, name='api_students_groups'),
+]
