42af700925ee4a2ebabd160aaf63f6add2686991
NickLavr <registerk@lavrenov.net>
2019-04-21 21:51:29 +0300

    Add DeadlineCard component and exp page with it
    Change-Id: I2ddf21af06b9a03322a4298d2dcd94755dca61bc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
+import Route from 'react-router-dom/Route';
+import Exp from './exp';
+
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.jsx'
+import {Switch} from "react-router";
 
 
-class App extends Component {
-  render() {
-    return (
-        <>
-        <NavBar />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
-        </>
-    );
-  }
+class SimplePage extends Component {
+    render() {
+        return (
+            <div className="App">
+                <NavBar/>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+            </div>
+        );
+    }
+}
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <>
+                    <Switch>
+                        <Route
+                            path='/exp' component={Exp}/>
+                        <Route
+                            path='/'
+                            component={SimplePage}/>
+                    </Switch>
+                </>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
