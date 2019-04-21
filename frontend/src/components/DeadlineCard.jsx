571edbe349845524f6f47709d4b82bf93f1dc0f2
NickLavr <registerk@lavrenov.net>
2019-04-21 21:51:29 +0300

    Add DeadlineCard component and exp page with it
    Change-Id: I2ddf21af06b9a03322a4298d2dcd94755dca61bc

---
+++ frontend/src/components/DeadlineCard.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AttachmentIcon from '@material-ui/icons/AttachFile';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
+const styles = {
+    card: {
+        minWidth: 275,
+        maxWidth: 600,
+        boxSizing: 'inherit',
+        borderRadius: 30,
+    },
+    date: {
+        maxWidth: 300,
+        marginLeft: 'auto',
+        display: 'inline-block',
+    },
+    done: {
+        display: 'inline-block',
+    },
+    caption: {
+        display: 'inline-block',
+    }
+};
+
+
+
+class DeadlineCard extends React.Component {
+    render() {
+        const {classes, text, caption, files, date, is_done} = this.props;
+        let files_buttons = <div>
+            {files.map(tile => (
+                <Button variant="contained" style={{margin: '10px'}}>
+                    <AttachmentIcon/>
+                    {tile}
+                </Button>))}
+        </div>;
+        return (
+            <Card className={classes.card}>
+                <CardContent>
+                    <div style={{display: 'flex'}}>
+                        <Typography className={classes.caption} variant="h5" component="h2">
+                            {caption}
+                        </Typography>
+                        <Typography className={classes.date}>
+                            {Date(date)}
+                        </Typography>
+                        <Typography className={classes.done}>
+                            <Checkbox checked={is_done} enabled={false}/>
+                            Done
+                        </Typography>
+                    </div>
+                    <hr style={{clear: 'both'}}/>
+                    <Typography component="p">
+                        {text}
+                        <br/>
+                    </Typography>
+                    <hr/>
+                    {files_buttons}
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+DeadlineCard.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(DeadlineCard);
