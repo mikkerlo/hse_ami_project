65fba01a85e3233cf94f9f4b2a9b4b631400ab4f
NickLavr <registerk@lavrenov.net>
2019-04-24 22:11:52 +0300

    add experiment with deadline card api loading
    Change-Id: If9689363215eef0bfad7cc18ef2615c77cbe29ef

---
+++ frontend/src/components/DeadlineCardList.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import DeadlineCard from "./DeadlineCard";
+
+class DeadlineCardList extends React.Component {
+    constructor(props) {
+        super(props);
+        let deadlines = [];
+        const self = this;
+        this.state = {};
+        this.state.deadlines = deadlines;
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("GET", "/api/deadlines");
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                try {
+                    let result = JSON.parse(xhr.response);
+                    self.setState({deadlines: result.response});
+                } catch (e) {
+                    console.log('error occured')
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    render() {
+        return <div>
+            {this.state.deadlines.map(deadline => (
+                <DeadlineCard
+                    text={deadline.content}
+                    caption={deadline.header}
+                    files={[]}
+                    date={deadline.valid_until}
+                />
+            ))}
+        </div>;
+    }
+}
+
+export default (DeadlineCardList);
