ab858847db68af6fcd9e81a26eae0b63d3e56413
Solonkov Denis <solonkovda@gmail.com>
2019-05-26 19:26:59 +0300

    Added invite tokens
    Change-Id: I23b01ae9617c35a82c98e955f50e473ff7370760

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -85,6 +85,7 @@ class Group(Model):
         short_name  (string):  Group short name, used to search in database
         description (string):  Group description, provided by group creator
         is_hidden   (bool):    If is true group is not shown on result page
+        invite_token (string): Invite token for students to join
         students      (Student): Ids of students that are linked to this group
     """
     full_name = models.CharField(max_length=255)
@@ -92,11 +93,16 @@ class Group(Model):
     description = models.TextField(blank=True)
     is_hidden = models.BooleanField()
     students = models.ManyToManyField(Student)
+    invite_token = models.CharField(
+        max_length=256, null=True, default=None, db_index=True)
     student_creator = models.ForeignKey(
         Student, on_delete=models.SET_NULL, null=True,
         related_name='groups_created')
     moderators = models.ManyToManyField(
         Student, related_name='groups_moderated')
+
+    def generate_invite_token(self):
+        self.invite_token = secrets.token_urlsafe(64)
 
     def apply_json(self, data):
         self.full_name = data.get('full_name', self.full_name)
@@ -171,17 +177,20 @@ class ContentElement(Model):
     content = models.TextField(blank=True)
     content_file = models.ManyToManyField(File, blank=True)
 
+    def apply_files(self, data):
+        if not 'files' in data:
+            return
+        files_id = []
+        for f in data['files']:
+            files_id.append(f['id'])
+        files = File.objects.filter(id__in=files_id)
+        self.content_file.clear()
+        self.content_file.add(*files)
+
     def apply_json(self, data):
         self.created_at = data['created_at']
         self.header = data['header']
         self.content = data['content']
-        if 'files' in data:
-            files_id = []
-            for f in data['files']:
-                files_id.append(f['id'])
-            files = File.objects.filter(id__in=files_id)
-            self.content_file.clear()
-            self.content_file.add(*files)
 
     def to_json(self):
         files = self.content_file.all()
@@ -280,7 +289,7 @@ class AuthToken(Model):
 
     def is_expired(self):
         current_time = int(time.time())
-        return self.last_access + settings.API_TOKEN_EXPIRE >= current_time
+        return self.last_access + settings.API_TOKEN_EXPIRE < current_time
 
     def to_json(self):
         return {
