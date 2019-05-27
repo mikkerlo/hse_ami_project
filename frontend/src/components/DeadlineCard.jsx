5635c9a741f0d5ae73f621392a064b9e1fdaf036
NickLavr <registerk@lavrenov.net>
2019-05-27 15:35:38 +0300

    few final fixed (deadline card rewrite)
    Change-Id: I7500c108864d5b5d078f01fec50e3ca6277b50e9

--- frontend/src/components/DeadlineCard.jsx
+++ frontend/src/components/DeadlineCard.jsx
@@ -4,10 +4,12 @@ import {withStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import EditIcon from '@material-ui/icons/Edit';
 import Typography from '@material-ui/core/Typography';
 import AttachmentIcon from '@material-ui/icons/AttachFile';
 import Checkbox from '@material-ui/core/Checkbox';
-
+import Link from "@material-ui/core/Link";
+import dateFormat from 'dateformat';
 
 const styles = {
     card: {
@@ -27,19 +29,29 @@ const styles = {
     },
     caption: {
         display: 'inline-block',
+    },
+    group_name: {
+        display: 'inline-block',
     }
 };
 
 
 class DeadlineCard extends React.Component {
     render() {
-        const {classes, text, caption, files, date, is_done} = this.props;
+        const {classes, text, caption, files, date, is_done, group_name, id} = this.props;
         let files_buttons = <div>
             {files.map(file => (
-                <Button variant="contained" style={{margin: '10px'}} key={file}>
+                <Button
+                    variant="contained"
+                    style={{margin: '10px'}}
+                    key={file.id}
+                    onClick={() => window.location = file.url}
+                >
                     <AttachmentIcon/>
-                    {file}
-                </Button>))}
+                    {file.name}
+                </Button>
+            ))
+            }
         </div>;
         if (files.length > 0) {
             files_buttons = <div>
@@ -55,12 +67,18 @@ class DeadlineCard extends React.Component {
                             {caption}
                         </Typography>
                         <Typography className={classes.valid_until}>
-                            {(new Date(date * 1000)).toString()}
+                            {dateFormat(new Date(date * 1000), "HH:MM hh.mm yyyy")}
                         </Typography>
-                        <Typography className={classes.done}>
-                            <Checkbox checked={is_done} enabled={"false"}/>
-                            Done
+                        <Typography  className={classes.group_name}>
+                            {group_name}
                         </Typography>
+                        {/*<Typography className={classes.done}>*/}
+                        {/*    <Checkbox checked={is_done} enabled={"false"}/>*/}
+                        {/*    Done*/}
+                        {/*</Typography>*/}
+                        <Button onClick={() => window.location = '/deadline/'+ id + '/edit'}>
+                        <EditIcon/>
+                        </Button>
                     </div>
                     <hr style={{clear: 'both'}}/>
                     <Typography component="p">
--- frontend/src/components/DeadlineCardList.jsx
+++ frontend/src/components/DeadlineCardList.jsx
@@ -37,11 +37,13 @@ class DeadlineCardList extends React.Component {
         return <div style={{width: '100%'}}>
             {this.state.deadlines.map(deadline => (
                 <DeadlineCard
+                    id={deadline.id}
                     text={deadline.content}
                     caption={deadline.header}
-                    files={[]}
+                    files={deadline.files}
                     date={deadline.valid_until}
                     key={deadline.id}
+                    group_name={deadline.group_name}
                 />
             ))}
         </div>;
